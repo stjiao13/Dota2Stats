368849fd1ced90b85a5cb57f0ec7bd1cb0bbbba1
Shutong Jiao <a441676131@gmail.com>
2018-10-07 05:40:06 +0000

    basic layout asnd routing

--- app.js
+++ app.js
@@ -1,31 +1,87 @@
-var express = require("express")
+var express = require("express");
 var app = express();
+var bodyParser  = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine","ejs");
+
+
+const { Pool, Client } = require('pg')
+const connectionString = "postgres://qgoyzerrrddgkd:115fc521e731b0eb673a50366b03e6bacd5d1cb2209f88d49d5d5011df8781b7@ec2-54-83-27-165.compute-1.amazonaws.com:5432/d7pgeu1vs3a7r";
+const router = express.Router();
+const matches = [];
+
+const client = new Client({
+  connectionString: connectionString,
+  ssl: true,
+});
+
+client.connect();
 
 app.get("/",function(req,res) {
     // res.render("home");
     res.render("landing");
 });
 
+// get 
 app.get("/matches",function(req,res) {
-        var matches = [
-        {id:"1",time:"2"},
-        {id:"2",time:"3"},
-        ];
-    res.render("matches",{matches:matches});
+
+    client.query('SELECT * FROM matches;',(err,matches) => {
+        if (err) {
+            console.log(err.stack);
+        } else {
+            // console.log(matches.rows);
+            res.render("matches",{matches:matches.rows})
+        }
+    });
+
 });
+
+app.get("/matches/:id",function(req, res) {
+    const text1 = 'select * from teamfights where match_id = ' + req.params.id +';';
+    // console.log(req.params.id);
+    client.query(text1, (err1,teamfights) => {
+        if (err1) {
+            console.log(err1.stack);
+        } else {
+            // console.log(teamfights);
+            // res.render("show",{teamfights : teamfights.rows})
+            const text2 = 'select * from players where match_id = ' + req.params.id +';';
+            client.query(text2,(err2,players) => {
+                if(err2) {
+                    console.log(err2.stack);
+                }
+                else{
+                    // console.log(players.rows);
+                    res.render("show",{
+                        teamfights:teamfights.rows,
+                        players:players.rows
+                    })
+                }
+            })
+            
+        }
+    })
+    // res.send(text);
+    
+})
+app.get("/purchases/:id",function(req, res) {
+    const id = req.params.id;
+    const arr = id.split('_');
+    const text = 'select * from purchase_log where match_id = ' + arr[0]+' AND player_slot = ' + arr[1];
+    client.query(text,(err,purchase_log) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("purchases",{purchase_log : purchase_log.rows});
+        }
+    })
+})
+
 
 
 app.listen(process.env.PORT,process.env.IP,() =>{
     console.log("started");
 });
 
-// const { Client } = require('pg');
-
-// const client = new Client({
-//   connectionString: 'postgres://lsnkyahhknfvoa:5465e49f9d8cc79c2398324b61fa4ae81a14e2758c64ad912ebc8b0e3687d2bc@ec2-54-225-68-133.compute-1.amazonaws.com:5432/dackksvkda5i3v',
-//   ssl: true,
-// });
-
-// client.connect();
