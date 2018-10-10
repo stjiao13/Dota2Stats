6779486c91463afca542591dc8426e7c8d6219cc
Shutong Jiao <a441676131@gmail.com>
2018-10-10 06:51:38 +0000

    update .gitignore

--- model/sql_script/purchase_log.sql
+++ model/sql_script/purchase_log.sql
@@ -6,5 +6,7 @@ CREATE TABLE purchase_log
   match_id serial NOT NULL,
   PRIMARY KEY (match_id,player_slot,time,item_id),
   CONSTRAINT constraint_fk1 FOREIGN KEY (match_id) REFERENCES matches(match_id),
-  CONSTRAINT constraint_fk2 FOREIGN KEY (item_id) REFERENCES item_id(id)
+  CONSTRAINT constraint_fk2 FOREIGN KEY (item_id) REFERENCES item_id(id),
+  CONSTRAINT constraint_fk3 FOREIGN KEY (match_id,player_slot) REFERENCES players(match_id,player_slot);
+
 );
